c8048453d9ed71a06e9b5826d89c48aff575dd94
oliver-0 <36753582+oliver-0@users.noreply.github.com>
2018-03-26 20:39:32 +0100

    Radiobox change
    Before, the tabs would show/hide using the onclick attribute of the <input> tags.
    Now I've moved the onclick event handler into radiobox.js, and it is applied on DOMContentLoaded.

--- radiobox.js
+++ radiobox.js
@@ -1,3 +1,32 @@
+//Try moving all of the scripts out of the HTML. You can use the below function to initialise some event listeners
+// try using a function to make all of the boxes hidden (use getElementsbyclassname? idk) and then use 'this' to get the clicked one to show up
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    alert("Ready!");
+}, false);
+*/
+
+document.addEventListener('DOMContentLoaded', function() {
+  var buttons = document.getElementsByClassName('radiobox-button')
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function() {
+      ////hides all of the boxes (yearly, monthly, weekly)
+      var boxes = document.getElementsByClassName('radiobox')
+      for (var j = 0; j < boxes.length; j++) {
+        boxes[j].style.display = "none";
+      }
+      ////shows the box for which the corresponding radio button was clicked
+      //document.getElementById('yearly-box').style.display = "none";
+      document.getElementById(this.value).style.display = 'block';
+      console.log(this.value);
+
+    }
+  }
+});
+
+
+/**old functionality:
+
 function showyearly() {
   document.getElementById("yearly-box").style.display = "block";
   document.getElementById("monthly-box").style.display = "none";
@@ -15,3 +44,4 @@ function showweekly() {
   document.getElementById("monthly-box").style.display = "none";
   document.getElementById("weekly-box").style.display = "block";
 }
+**/
