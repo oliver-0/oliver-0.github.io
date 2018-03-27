f1ed70080d614162a80721d237021516290cd732
oliver-0 <36753582+oliver-0@users.noreply.github.com>
2018-03-27 20:31:36 +0100

    Updated radiobox
    Condensed radiobox.js into a more readable format, added basic styling to the output box.

--- radiobox.js
+++ radiobox.js
@@ -1,40 +1,18 @@
 //On DOMContentLoaded, adds onclick functions to each of the radiobox buttons.
 // The onclick hides all three output boxes, then shows the corresponding one.
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function addRadioboxOnclicks() {
   var buttons = document.getElementsByClassName('radiobox-button')
   for (var i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function() {
-      //hides all of the boxes (yearly, monthly, weekly)
-      var boxes = document.getElementsByClassName('radiobox')
-      for (var j = 0; j < boxes.length; j++) {
-        boxes[j].style.display = "none";
-      }
-      //shows the box for which the corresponding radio button was clicked
-      document.getElementById(this.value).style.display = 'block';
-    }
+    buttons[i].onclick = showBox;
   }
 });
 
-
-
-/* - - - - - - - - - - - */
-/**old functionality:
-
-function showyearly() {
-  document.getElementById("yearly-box").style.display = "block";
-  document.getElementById("monthly-box").style.display = "none";
-  document.getElementById("weekly-box").style.display = "none";
+function showBox() {
+  //hides all of the radioboxes (yearly, monthly, weekly)
+  var boxes = document.getElementsByClassName('radiobox')
+  for (var j = 0; j < boxes.length; j++) {
+    boxes[j].style.display = "none";
+  }
+  //shows the box for which the corresponding radio button was clicked
+  document.getElementById(this.value).style.display = 'block';
 }
-
-function showmonthly() {
-  document.getElementById("yearly-box").style.display = "none";
-  document.getElementById("monthly-box").style.display = "block";
-  document.getElementById("weekly-box").style.display = "none";
-}
-
-function showweekly() {
-  document.getElementById("yearly-box").style.display = "none";
-  document.getElementById("monthly-box").style.display = "none";
-  document.getElementById("weekly-box").style.display = "block";
-}
-**/
